d507a41e2b502e62f55bd9582e3535331910c530
ipkathon04 <43269041+ipkathon04@users.noreply.github.com>
2018-10-19 15:21:26 +0200

    Add files via upload

---
+++ semiUBLU/futurm.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Bootstrap 4, from LayoutIt!</title>
+
+        <meta name="description" content="Source code generated using layoutit.com">
+        <meta name="author" content="LayoutIt!">
+
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="row">
+                        <div class="col-8">
+                            <h1 class="centerH centerHH">
+                                ROBOTSHITÉ
+                            </h1>
+                        </div>
+                        <div class="col-4">
+                            <img alt="Bootstrap Image Preview"  style="height: 150px;width: 150px;" src="image/ipi.jpg" class="rounded center">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-12" style="margin-top: 190px">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="card" id="cdpform">
+                            <div class="card-block">
+                                <h5 class="card-title centerFM" style="text-align: center">
+                                    Chef de projet informatique
+                                </h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card" id="devweb">
+                            <div class="card-block">
+                                <h5 class="card-title centerFM" style="text-align: center">
+                                    Développeur WEB
+                                </h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card" id="devfs">
+                            <div class="card-block">
+                                <h5 class="card-title centerFM" style="text-align: center">
+                                    Développeur fullstack
+                                </h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card" id="adminr">
+                            <div class="card-block">
+                                <h5 class="card-title centerFM" style="text-align: center">
+                                    ADMINISTRATEUR RÉSEAU
+                                </h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card" id="cdpr">
+                            <div class="card-block">
+                                <h5 class="card-title centerFM" style="text-align: center">
+                                    CHEF DE PROJET SI
+                                </h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card" id="techr">
+                            <div class="card-block">
+                                <h5 class="card-title centerFM" style="text-align: center">
+                                    TECHNICIEN RÉSEAU
+                                </h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mmIR" id="vis">
+                    <div class="jumbotron" style="width: 100%">
+                        <h4 class="centerFM" style="text-align: center; margin-bottom: 20px">
+                            FORMATION PROPOSÉE : 
+                        </h4>
+                        <h3 class='centerFM' id="titleform" style="text-align: center">
+                            
+                        </h3>
+                        <p id="contenuform" class="centerFM" style="text-align: center">
+                            
+                        </p>
+                        <p>
+                            <a class="btn btn-primary btn-large centerFM" id="" href="http://www.ipi-ecoles.com/digitalents-expert-developpement-logiciel/">EN SAVOIR PLUS</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/scripts.js"></script>
+</body>
+</html>
---
+++ semiUBLU/index.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Bootstrap 4, from LayoutIt!</title>
+
+        <meta name="description" content="Source code generated using layoutit.com">
+        <meta name="author" content="LayoutIt!">
+
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="row">
+                        <div class="col-8">
+                            <h1 class="centerH centerHH">
+                                ROBOTSHITÉ
+                            </h1>
+                        </div>
+                        <div class="col-4">
+                            <img alt="Bootstrap Image Preview"  style="height: 150px;width: 150px;" src="image/ipi.jpg" class="rounded center">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row" style="margin-top: 100px">
+                <div class="col-12">
+                    <div class="row" onclick="location.href = 'visite.php'" style="cursor: pointer">
+                        <div class="col-6">
+                            <h1 class="centerH center">
+                                Visite du campus
+                            </h1>
+                        </div>
+                        <div class="col-6 imgI">
+                            <img alt="Bootstrap Image Preview" src="image/campus.jpg" class="rounded center responsive">
+                        </div>
+                    </div>
+                    <div class="row" onclick="location.href = 'visite.php'">
+                        <div class="col-6 imgI">
+                            <img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" class="rounded center responsive">
+                        </div>
+                        <div class="col-6">
+                            <h1 class="centerH center">
+                                Les formations
+                            </h1>
+                        </div>
+                    </div>
+                    <div class="row" onclick="location.href = 'futurm.php'">
+                        <div class="col-6" style="margin-bottom: 20px">
+                            <h1 class="centerH center">
+                                Votre futur métier
+                            </h1>
+                        </div>
+                        <div class="col-6 imgI">
+                            <img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" class="rounded center responsive">
+                        </div>
+                    </div>
+                    <div class="row" onclick="location.href = 'visite.php'">
+                        <div class="col-6 imgI">
+                            <img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" class="rounded center responsive">
+                        </div>
+                        <div class="col-6">
+                            <h4 class="centerH center">
+                                <b>les dossiers d'inscriptions</b>
+                            </h4>
+                        </div>
+                    </div>
+                    <div class="row" onclick="location.href = 'visite.php'">
+                        <div class="col-6">
+                            <h4 class="centerH center">
+                                <b>Prendre un rendez-vous</b>
+                            </h4>
+                        </div>
+                        <div class="col-6 imgI">
+                            <img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" class="rounded center responsive">
+                        </div>
+                    </div>
+                    <div class="row" onclick="location.href = 'avis.php'">
+                        <div class="col-6 imgI">
+                            <img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" class="rounded center responsive">
+                        </div>
+                        <div class="col-6">
+                            <h1 class="centerH center">
+                                Avis Étudiants
+                            </h1>
+                        </div>
+                    </div>
+                    <div class="row" onclick="location.href = 'qa.php'">
+                        <div class="col-6">
+                            <h1 class="centerH center">
+                                Vos questions
+                            </h1>
+                        </div>
+                        <div class="col-6 imgI">
+                            <img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" class="rounded center responsive">
+                        </div>
+                    </div>
+                    <div class="row" onclick="location.href = 'visite.php'">
+                        <div class="col-6 imgI">
+                            <img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" class="rounded center responsive">
+                        </div>
+                        <div class="col-6">
+                            <h1 class="centerH center">
+                                Vos questions
+                            </h1>
+                        </div>
+
+                    </div>
+                    <div class="row footer" style="margin-top: 20px">
+                        <div class="col-12 imgI">
+                            <img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" class="rounded center responsive">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
